403b2a9d4779feaa08e9c85f746060e4a86fb938
Lets Build Together <letsbuild2gether.business@gmail.com>
2025-03-12 15:29:31 +0100

    adding support for photo processing

--- controller/lib/telegram.js
+++ controller/lib/telegram.js
@@ -16,6 +16,32 @@ function sendMessage(chatId, messageText) {
     });
 }
 
+function getFile(fileId) {
+  return axiosInstance
+    .get("getFile", {
+      file_id: fileId,
+    })
+    .catch((ex) => {
+      errorHandler(ex, "getFile", "axios");
+    });
+}
+
+async function processThePhoto(messageObj) {
+  if (messageObj.photo && messageObj.photo.length !== 0) {
+    //Taking the file id from the photo
+    const fileId = messageObj.photo[messageObj.photo.length - 1].file_id;
+    //Getting the file data using that file id
+    const fileData = await getFile(fileId);
+    if (fileData.data && fileData.data.result) {
+      const fileName = fileData.data.result.file_path;
+      // This the public url, for the user sent photo we can use
+      const file_public_path = `https://api.telegram.org/file/bot${MY_TOKEN}/${fileName}`;
+      return true;
+    }
+  }
+  return false;
+}
+
 function isBotEnabledForThisChat(chatId) {
   //return ["chatId1","chatId2"].indexOf(chatId) !== -1;
   return true;
@@ -45,8 +71,16 @@ async function handleMessage(messageObj) {
         default:
           return sendMessage(chatId, "Hey hi, i don't know that command");
       }
-    } else {
+    } else if (messageObj.photo) {
+      const success = await processThePhoto(messageObj);
+      const message = success
+        ? "Photo recieved successfully"
+        : "Error while processing the photo";
+      return sendMessage(chatId, "Received your photo");
+    } else if (messageText) {
       return sendMessage(chatId, messageText);
+    } else {
+      return sendMessage(chatId, "Received your message!");
     }
   } catch (error) {
     errorHandler(error, "handleMessage");
